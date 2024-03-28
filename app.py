309e78ea340a853d06a8e9d849517edd131db482
TMRmehta <68707216+TMRmehta@users.noreply.github.com>
2024-03-28 17:12:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras import backend as K
 import streamlit as st
 import pickle
+import boto3
 def main():
     st.title("Login Page")
 
@@ -247,6 +248,8 @@ if authentication_status:
     #File Input
     uploaded_file = st.file_uploader("Upload Image")
     if uploaded_file is not None:
+      s3 = boto3.client('s3’, aws_access_key_id= st.secrets["aws"]["AWS_ACCESS_KEY_ID"], aws_secret_access_key=st.secrets["aws"]["AWS_SECRET_ACCESS_KEY"])
+      s3.upload_fileobj(uploaded_file, "neuroaid", "test")
       if (Model_option == 0):
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
         image = cv2.imdecode(file_bytes, 1)
